d8aba0c0fa467daaaafaa243cd73479f69106934
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-16 21:00:46 +0100

    Added function to check argument against statement < or >

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,15 +1,15 @@
 function comparisonToEqual(a) {
-    if(a < 5) {
-        return "Less than 5";
-    } 
-    if(5 <= a && a < 10) {
-        return "Less than 10";
-    }
-    if(10 <= a && a <= 20) {
-        return "10 or over";
-    }
     if(a > 20) {
         return "More than 20";
+    } 
+    if(a >= 10) {
+        return "10 or over";
+    }
+    if(a < 5) {
+        return "Less than 5";
+    }
+    if(a < 10) {
+        return "Less than 10";
     }
 }
 
